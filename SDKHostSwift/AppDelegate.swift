8cfb1be78e52d1f1b9f96ede3d6a23e776400294
davidSchuppa <d.schuppa@gmail.com>
2022-01-06 14:54:57 +0100

    feat(sdk-host): made sdk host application iOS 13 compatible SUITEDEV-29908
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>

---
+++ SDKHostSwift/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Emarsys on 2022. 01. 06..
+//
+
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
